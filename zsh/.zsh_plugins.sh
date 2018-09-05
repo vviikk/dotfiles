b7a77df58d68a3d30d867796544bcd73d85a56e6
Vik Ramanujam <piggyslasher@gmail.com>
2018-09-05 15:25:30 +0800

    Remove antigen

--- zsh/.zsh_plugins.sh
+++ zsh/.zsh_plugins.sh
@@ -1,41 +1,41 @@
-source /home/piggyslasher/.cache/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-lukechilds-SLASH-zsh-nvm/zsh-nvm.plugin.zsh
-fpath+=( /home/piggyslasher/.cache/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-lukechilds-SLASH-zsh-nvm )
-source /home/piggyslasher/.cache/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-robbyrussell-SLASH-oh-my-zsh/plugins/git/git.plugin.zsh
-fpath+=( /home/piggyslasher/.cache/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-robbyrussell-SLASH-oh-my-zsh/plugins/git )
-source /home/piggyslasher/.cache/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-Seinh-SLASH-git-prune/git-prune.plugin.zsh
-fpath+=( /home/piggyslasher/.cache/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-Seinh-SLASH-git-prune )
-source /home/piggyslasher/.cache/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-unixorn-SLASH-git-extra-commands/git-extra-commands.plugin.zsh
-fpath+=( /home/piggyslasher/.cache/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-unixorn-SLASH-git-extra-commands )
-source /home/piggyslasher/.cache/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-robbyrussell-SLASH-oh-my-zsh/plugins/pip/pip.plugin.zsh
-fpath+=( /home/piggyslasher/.cache/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-robbyrussell-SLASH-oh-my-zsh/plugins/pip )
-source /home/piggyslasher/.cache/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-robbyrussell-SLASH-oh-my-zsh/plugins/command-not-found/command-not-found.plugin.zsh
-fpath+=( /home/piggyslasher/.cache/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-robbyrussell-SLASH-oh-my-zsh/plugins/command-not-found )
-source /home/piggyslasher/.cache/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-j-arnaiz-SLASH-common-aliases/common-aliases.plugin.zsh
-fpath+=( /home/piggyslasher/.cache/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-j-arnaiz-SLASH-common-aliases )
+source /Users/vramanujam/Library/Caches/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-lukechilds-SLASH-zsh-nvm/zsh-nvm.plugin.zsh
+fpath+=( /Users/vramanujam/Library/Caches/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-lukechilds-SLASH-zsh-nvm )
+source /Users/vramanujam/Library/Caches/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-robbyrussell-SLASH-oh-my-zsh/plugins/git/git.plugin.zsh
+fpath+=( /Users/vramanujam/Library/Caches/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-robbyrussell-SLASH-oh-my-zsh/plugins/git )
+source /Users/vramanujam/Library/Caches/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-Seinh-SLASH-git-prune/git-prune.plugin.zsh
+fpath+=( /Users/vramanujam/Library/Caches/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-Seinh-SLASH-git-prune )
+source /Users/vramanujam/Library/Caches/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-unixorn-SLASH-git-extra-commands/git-extra-commands.plugin.zsh
+fpath+=( /Users/vramanujam/Library/Caches/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-unixorn-SLASH-git-extra-commands )
+source /Users/vramanujam/Library/Caches/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-robbyrussell-SLASH-oh-my-zsh/plugins/pip/pip.plugin.zsh
+fpath+=( /Users/vramanujam/Library/Caches/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-robbyrussell-SLASH-oh-my-zsh/plugins/pip )
+source /Users/vramanujam/Library/Caches/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-robbyrussell-SLASH-oh-my-zsh/plugins/command-not-found/command-not-found.plugin.zsh
+fpath+=( /Users/vramanujam/Library/Caches/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-robbyrussell-SLASH-oh-my-zsh/plugins/command-not-found )
+source /Users/vramanujam/Library/Caches/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-j-arnaiz-SLASH-common-aliases/common-aliases.plugin.zsh
+fpath+=( /Users/vramanujam/Library/Caches/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-j-arnaiz-SLASH-common-aliases )
 
-source /home/piggyslasher/.cache/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-robbyrussell-SLASH-oh-my-zsh/plugins/yarn/yarn.plugin.zsh
-fpath+=( /home/piggyslasher/.cache/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-robbyrussell-SLASH-oh-my-zsh/plugins/yarn )
-source /home/piggyslasher/.cache/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-robbyrussell-SLASH-oh-my-zsh/plugins/thefuck/thefuck.plugin.zsh
-fpath+=( /home/piggyslasher/.cache/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-robbyrussell-SLASH-oh-my-zsh/plugins/thefuck )
-source /home/piggyslasher/.cache/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-robbyrussell-SLASH-oh-my-zsh/plugins/z/z.plugin.zsh
-fpath+=( /home/piggyslasher/.cache/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-robbyrussell-SLASH-oh-my-zsh/plugins/z )
-source /home/piggyslasher/.cache/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-zsh-users-SLASH-zsh-history-substring-search/zsh-history-substring-search.plugin.zsh
-fpath+=( /home/piggyslasher/.cache/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-zsh-users-SLASH-zsh-history-substring-search )
-source /home/piggyslasher/.cache/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-zdharma-SLASH-fast-syntax-highlighting/fast-syntax-highlighting.plugin.zsh
-fpath+=( /home/piggyslasher/.cache/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-zdharma-SLASH-fast-syntax-highlighting )
-source /home/piggyslasher/.cache/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-zsh-users-SLASH-zsh-autosuggestions/zsh-autosuggestions.plugin.zsh
-fpath+=( /home/piggyslasher/.cache/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-zsh-users-SLASH-zsh-autosuggestions )
-source /home/piggyslasher/.cache/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-lukechilds-SLASH-zsh-better-npm-completion/zsh-better-npm-completion.plugin.zsh
-fpath+=( /home/piggyslasher/.cache/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-lukechilds-SLASH-zsh-better-npm-completion )
-source /home/piggyslasher/.cache/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-akoenig-SLASH-npm-run.plugin.zsh/npm-run.plugin.zsh
-fpath+=( /home/piggyslasher/.cache/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-akoenig-SLASH-npm-run.plugin.zsh )
-source /home/piggyslasher/.cache/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-zsh-users-SLASH-zsh-completions/zsh-completions.plugin.zsh
-fpath+=( /home/piggyslasher/.cache/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-zsh-users-SLASH-zsh-completions )
-source /home/piggyslasher/.cache/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-unixorn-SLASH-docker-helpers.zshplugin/docker-helpers.plugin.zsh
-fpath+=( /home/piggyslasher/.cache/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-unixorn-SLASH-docker-helpers.zshplugin )
-source /home/piggyslasher/.cache/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-molovo-SLASH-tipz/tipz.zsh
-fpath+=( /home/piggyslasher/.cache/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-molovo-SLASH-tipz )
-source /home/piggyslasher/.cache/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-mafredri-SLASH-zsh-async/async.plugin.zsh
-fpath+=( /home/piggyslasher/.cache/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-mafredri-SLASH-zsh-async )
-source /home/piggyslasher/.cache/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-sindresorhus-SLASH-pure/pure.plugin.zsh
-fpath+=( /home/piggyslasher/.cache/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-sindresorhus-SLASH-pure )
+source /Users/vramanujam/Library/Caches/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-robbyrussell-SLASH-oh-my-zsh/plugins/yarn/yarn.plugin.zsh
+fpath+=( /Users/vramanujam/Library/Caches/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-robbyrussell-SLASH-oh-my-zsh/plugins/yarn )
+source /Users/vramanujam/Library/Caches/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-robbyrussell-SLASH-oh-my-zsh/plugins/thefuck/thefuck.plugin.zsh
+fpath+=( /Users/vramanujam/Library/Caches/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-robbyrussell-SLASH-oh-my-zsh/plugins/thefuck )
+source /Users/vramanujam/Library/Caches/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-robbyrussell-SLASH-oh-my-zsh/plugins/z/z.plugin.zsh
+fpath+=( /Users/vramanujam/Library/Caches/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-robbyrussell-SLASH-oh-my-zsh/plugins/z )
+source /Users/vramanujam/Library/Caches/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-zsh-users-SLASH-zsh-history-substring-search/zsh-history-substring-search.plugin.zsh
+fpath+=( /Users/vramanujam/Library/Caches/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-zsh-users-SLASH-zsh-history-substring-search )
+source /Users/vramanujam/Library/Caches/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-zdharma-SLASH-fast-syntax-highlighting/fast-syntax-highlighting.plugin.zsh
+fpath+=( /Users/vramanujam/Library/Caches/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-zdharma-SLASH-fast-syntax-highlighting )
+source /Users/vramanujam/Library/Caches/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-zsh-users-SLASH-zsh-autosuggestions/zsh-autosuggestions.plugin.zsh
+fpath+=( /Users/vramanujam/Library/Caches/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-zsh-users-SLASH-zsh-autosuggestions )
+source /Users/vramanujam/Library/Caches/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-lukechilds-SLASH-zsh-better-npm-completion/zsh-better-npm-completion.plugin.zsh
+fpath+=( /Users/vramanujam/Library/Caches/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-lukechilds-SLASH-zsh-better-npm-completion )
+source /Users/vramanujam/Library/Caches/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-akoenig-SLASH-npm-run.plugin.zsh/npm-run.plugin.zsh
+fpath+=( /Users/vramanujam/Library/Caches/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-akoenig-SLASH-npm-run.plugin.zsh )
+source /Users/vramanujam/Library/Caches/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-zsh-users-SLASH-zsh-completions/zsh-completions.plugin.zsh
+fpath+=( /Users/vramanujam/Library/Caches/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-zsh-users-SLASH-zsh-completions )
+source /Users/vramanujam/Library/Caches/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-unixorn-SLASH-docker-helpers.zshplugin/docker-helpers.plugin.zsh
+fpath+=( /Users/vramanujam/Library/Caches/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-unixorn-SLASH-docker-helpers.zshplugin )
+source /Users/vramanujam/Library/Caches/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-molovo-SLASH-tipz/tipz.zsh
+fpath+=( /Users/vramanujam/Library/Caches/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-molovo-SLASH-tipz )
+source /Users/vramanujam/Library/Caches/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-mafredri-SLASH-zsh-async/async.plugin.zsh
+fpath+=( /Users/vramanujam/Library/Caches/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-mafredri-SLASH-zsh-async )
+source /Users/vramanujam/Library/Caches/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-sindresorhus-SLASH-pure/pure.plugin.zsh
+fpath+=( /Users/vramanujam/Library/Caches/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-sindresorhus-SLASH-pure )
--- zsh/.zshrc
+++ zsh/.zshrc
@@ -40,3 +40,4 @@ source ~/.bash_functions
 
 export FZF_DEFAULT_COMMAND='ag --hidden --ignore .git -g ""'
 [ -f ~/.fzf.zsh ] && source ~/.fzf.zsh
+
