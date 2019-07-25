e54c36834cdf0dd83a7a0acc94c290e4f973205d
Vik Ramanujam <vik@pop-os.localdomain>
2019-07-25 20:12:43 +0100

    Add install spaceneovim with local config

---
+++ nvim/install-spaceneovim.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+
+CONFIG_DIR="$HOME/.config/nvim"
+CONFIG_FILE="$HOME/.config/nvim/init.vim"
+AUTOLOAD_DIR="$HOME/.config/nvim/autoload"
+AUTOLOAD_FILE="$HOME/.config/nvim/autoload/spaceneovim.vim"
+CACHE_DIR="$HOME/.cache/nvim"
+
+# Backup exisiting configuration.
+if [ -d "$CONFIG_DIR" ]; then
+    backup_time=$(date +'%Y-%m-%d.%H-%M-%S')
+      mv "$CONFIG_DIR" "$CONFIG_DIR.$backup_time.backup"
+    fi
+
+    # Create the neccessary directories, download the configuration files and launch
+    # Neovim.
+    mkdir -p "$CONFIG_DIR" \
+	  && mkdir -p "$CACHE_DIR" \
+	  # && echo ">>> Downloading configuration file" \
+	  # && curl -sSfL https://raw.githubusercontent.com/tehnix/spaceneovim/master/vimrc.sample.vim -o "$CONFIG_FILE" \
+	  && echo ">>> Copying your init.vim" \
+      && cp ./.config/nvim/init.vim "$CONFIG_FILE" \
+	  && echo ">>> Creating autoload directory for spaceneovim" \
+	  && mkdir -p "$AUTOLOAD_DIR" \
+	  && echo ">>> Downloading spaceneovim core" \
+	  && curl -sSfL https://raw.githubusercontent.com/tehnix/spaceneovim/master/autoload/spaceneovim.vim -o "$AUTOLOAD_FILE" \
+	  && echo ">>> Launching nvim" \
+	  && nvim --cmd "let g:dotspaceneovim_do_not_run_bootstrap=1" +SpaceNeovimRunInstallProcess && nvim \
+	  && echo ">>> DONE!"
