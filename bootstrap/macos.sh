45b043dd9996187409d9433473115a32aff1a8b1
Vik Ramanujam <piggyslasher@Viks-MacBook-Pro.local>
2020-06-29 19:06:10 +0100

    Update Brefile

--- bootstrap/macos.sh
+++ bootstrap/macos.sh
@@ -1,5 +1,11 @@
 #!/bin/sh
 
+# Jason Rudolph's Keyboard 
+git clone https://github.com/jasonrudolph/keyboard.git ~/.keyboard
+cd ~/.keyboard
+script/setup
+
+# Neovim / Spaceneovim
 brew install neovim && \                     
 sudo pip3 install neovim && \
 sudo pip3 install --upgrade neovim && \
