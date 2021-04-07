ef74aa38ceb01130b5624861d0ebb6d93f0bf609
Vik Ramanujam <Vik@ramanuj.am>
2021-04-07 12:48:30 +0200

    Add exa aliases to zsh

--- antibody/install.sh
+++ antibody/install.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-antibody bundle < "$HOME/dotfiles/antibody/.zsh_plugins.txt" > $HOME/.zsh_plugins.sh
+antibody bundle < "$HOME/.dotfiles/antibody/.zsh_plugins.txt" > $HOME/.zsh_plugins.sh
 antibody update
